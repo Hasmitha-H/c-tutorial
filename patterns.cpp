28cb8413883fd2ac6ea3a21276ef68463b563aef
Hasmitha-H <70689516+Hasmitha-H@users.noreply.github.com>
2021-07-29 14:15:32 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -36,56 +36,56 @@ int main()
     //     cout<<endl;
     // }
 
-    // print numbers pattern
-    //   int n,count=1;
-    // cin>>n;
-    // for(int i=1;i<=n;i++)
-    // {
-    //     for(int j=1;j<=i;j++)
-    //         {
-    //              cout<<count<<" ";
-    //             //  count++;
-    //         }
-    //     cout<<endl;
-    // }
+//     print numbers pattern
+      int n,count=1;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+            {
+                 cout<<count<<" ";
+                //  count++;
+            }
+        cout<<endl;
+    }
 
     // print 0 and 1 problem
-    // int n;
-    // cin>>n;
-    // for(int i=1;i<=n;i++)
-    // {
-    //     for(int j=1;j<=i;j++)
-    //     {
-    //         if((i+j)%2==0)
-    //         {
-    //             cout<<"1";
-    //         }
-    //         else
-    //         {
-    //             cout<<"0";
-    //         }
-    //     }
-    //     cout<<endl;
-    // }
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            if((i+j)%2==0)
+            {
+                cout<<"1";
+            }
+            else
+            {
+                cout<<"0";
+            }
+        }
+        cout<<endl;
+    }
 
 
 
 // rombus pattern
-    // int n;
-    // cin >> n;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= n - i; j++)
-    //     {
-    //         cout << " ";
-    //     }
-    //     for (int j = 1; j <= n; j++)
-    //     {
-    //         cout << "*";
-    //     }
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
 
-    //     cout << endl;
-    // }
+        cout << endl;
+    }
 
 
 
